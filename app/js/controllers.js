008aff6693f29b1581af4f21e1f81ec85def12b2
Rahul Ramki <rahul.ramki@yahoo.com>
2014-08-21 17:22:45 -0400

    =lskdhf

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,13 @@ angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
  $scope.difference = 0;
   
-  $scope.getDiff = function() {
-    $scope.difference = Math.abs(parseInt($scope.timezone2) - parseInt($scope.timezone));
-  }
+ 
+ $scope.buttonpress=false;
+ $scope.getDiff = function() 
+{
+    
+$scope.difference = Math.abs(parseFloat($scope.timezone2) - parseFloat($scope.timezone));
+ $scope.buttonpress=true; }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
