1c85aa24b0c526f72cd8c940a056691984cceefb
Rahul Ramki <rahul.ramki@yahoo.com>
2014-08-21 16:38:51 -0400

    sndfl

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,11 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
+ $scope.difference = 0;
+  
+  $scope.getDiff = function() {
+    $scope.difference = Math.abs(parseInt($scope.timezone2) - parseInt($scope.timezone));
+  }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
